3bd3162c05654df684fe1ddbb7de33e3e3d7a64c
Lakshay <108230497+Hyouteki@users.noreply.github.com>
2023-04-22 20:06:21 +0530

    Add files via upload

---
+++ zuber-include.sql
@@ -0,0 +1,6 @@
+use zuber;
+source /home/lakshaychauhan/Lakshay/MySQL/Zuber/zuber-schema.sql
+source /home/lakshaychauhan/Lakshay/MySQL/Zuber/zuber-data.sql
+source /home/lakshaychauhan/Lakshay/MySQL/Zuber/zuber-queries.sql
+source /home/lakshaychauhan/Lakshay/MySQL/Zuber/zuber-triggers.sql
+source /home/lakshaychauhan/Lakshay/MySQL/Zuber/zuber-olap.sql
